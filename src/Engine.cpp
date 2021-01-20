22f611bee363ab6c31ed9a5444397b36e345b15c
tylarboyle <tylar.boyle@gmail.com>
2021-01-20 14:52:51 -0700

    Added Engine class stub (test)

---
+++ src/Engine.cpp
@@ -0,0 +1,10 @@
+
+#include "Engine.h"
+
+Engine::Engine() {
+
+}
+
+Engine::~Engine() {
+
+}
---
+++ src/Engine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+#include <glm/glm.hpp>
+#include <memory>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+class Engine
+{
+public:
+	Engine(const std::string& objPath);
+	~Engine();
+	
+private:
+	
+};
