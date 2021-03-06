d30cc82e8aea630204bc3e7e6ec0c1152d9f8a38
Joshua Plosz <joshua.plosz@gmail.com>
2021-03-06 09:00:21 -0700

    Added the other 2 ai vehicles and set their color and temp positions

--- src/TeamStats.cpp
+++ src/TeamStats.cpp
@@ -4,11 +4,15 @@ namespace teamStats
 {
 	const std::unordered_map<Teams, glm::vec4> colors = {
 		{Teams::TEAM0, glm::vec4(.3f, .3f, 1.f, 0.f)},
-		{Teams::TEAM1, glm::vec4(.8f, .8f, .3f, 0.f)}
+		{Teams::TEAM1, glm::vec4(.9f, .9f, .3f, 0.f)},
+		{Teams::TEAM2, glm::vec4(.2f, .96f, .34f, 0.f)},
+		{Teams::TEAM3, glm::vec4(.83f, .2f, .92f, 0.f)}
 	};
 
 	std::unordered_map<Teams, unsigned int> scores = {
 		{Teams::TEAM0, 0},
 		{Teams::TEAM1, 0},
+		{Teams::TEAM2, 0},
+		{Teams::TEAM3, 0}
 	};
 }
