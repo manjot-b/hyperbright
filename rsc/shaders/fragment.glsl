57ba38531f5f42109d7d26596c18fd8bc53501bf
Manjot <mbal29@hotmail.com>
2021-03-14 20:59:19 -0600

    Move phong shading to its own function in fragment shader.

--- rsc/shaders/fragment.glsl
+++ rsc/shaders/fragment.glsl
@@ -19,7 +19,7 @@ in vec4 instanceColor;
 
 uniform sampler2D tex;
 uniform bool hasTexture;
-uniform vec3 pointOfView;
+uniform vec3 cameraPos;
 uniform bool isInstanceColor;
 uniform vec4 color;
 uniform float diffuseCoeff;
@@ -30,6 +30,41 @@ uniform Light lights[MAX_LIGHTS];
 uniform int lightCount;
 
 out vec4 fragColor;
+
+/*
+	Returns the total light at this fragment using the Phong shading model.
+*/
+vec4 phong()
+{
+	//basic phong shading
+	vec3 ambient = vec3(.1f);
+	vec3 diffuse = vec3(.0f);
+	vec3 specular = vec3(.0f);
+
+	vec3 viewDir = normalize(cameraPos - vertexPos);
+	vec3 unitNormal = normalize(normal);
+
+	for (int i = 0; i < lightCount; i++)
+	{
+		float attenuation = 1;
+		vec3 lightDir;
+		if (lights[i].isPoint) {
+			lightDir = normalize(lights[i].position - vertexPos);
+			float dist = length(lights[i].position - vertexPos);
+			attenuation = 1.f / (lights[i].constant + lights[i].linear * dist + lights[i].quadratic * dist * dist);
+		} else {
+			lightDir = normalize(-lights[i].position);
+		}
+			
+		diffuse += max(dot(lightDir, unitNormal), 0.0) * diffuseCoeff * lights[i].color * attenuation;
+		vec3 reflect = 2 * dot(lightDir, unitNormal) * unitNormal - lightDir;
+		reflect = normalize(reflect);
+		specular += pow(max(dot(reflect, viewDir), 0.0), shininess) * specularCoeff * lights[i].color * attenuation;
+	}
+
+	vec4 finalLight = vec4((ambient + diffuse + specular), 1.f);
+	return finalLight;
+}
 
 void main()
 {
@@ -43,32 +78,8 @@ void main()
 		else
 			vertexColor = color;
 
-		//basic phong shading
-		vec3 ambient = vec3(.1f);
-		vec3 diffuse = vec3(.0f);
-		vec3 specular = vec3(.0f);
-
-		vec3 viewDir = normalize(pointOfView - vertexPos);
-		for (int i = 0; i < lightCount; i++)
-		{
-			float attenuation = 1;
-			vec3 lightDir;
-			if (lights[i].isPoint) {
-				lightDir = normalize(lights[i].position - vertexPos);
-				float dist = length(lights[i].position - vertexPos);
-				attenuation = 1.f / (lights[i].constant + lights[i].linear * dist + lights[i].quadratic * dist * dist);
-			} else {
-				lightDir = normalize(-lights[i].position);
-			}
-			
-			diffuse += max(dot(lightDir, normal), 0.0) * diffuseCoeff * lights[i].color * attenuation;
-			vec3 reflect = 2 * dot(lightDir, normal) * normal - lightDir;
-			reflect = normalize(reflect);
-			specular += pow(max(dot(reflect, viewDir), 0.0), shininess) * specularCoeff * lights[i].color * attenuation;
-		}
-
-		vec4 finalLight = vec4((ambient + diffuse + specular), 1.f);
-		fragColor = finalLight * vertexColor;
+		
+		fragColor = phong() * vertexColor;
 	}
 	else
 	{
